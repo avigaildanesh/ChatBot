5e0bdcbe1052b65885d7e9c50e5231ab83913834
avigaildanesh <avigaildanesh100@gmail.com>
2025-06-10 11:14:45 +0300

    create user changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,19 @@
 const User = require('../models/userModel');
-// createUser function to handle user creation
+
 exports.createUser = async (req, res) => {
   try {
     const { name, phone } = req.body;
+
     if (!name.trim() || !/^\d{2,3}-?\d{6,8}$/.test(phone)) {
       return res.status(400).json({ error: 'Invalid name or phone' });
     }
 
-    const user = new User({ name: name.trim(), phone });
+    let user = await User.findOne({ phone });
 
-    await user.save()
-      .catch(err => console.error('Error saving user:', err));
+    if (!user) {
+      user = new User({ name: name.trim(), phone });
+      await user.save();
+    }
 
     res.json({ message: 'User saved', userId: user._id });
   } catch (e) {
@@ -18,4 +21,3 @@ exports.createUser = async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 };
-
